e0764204e55dd8cc63c2e177125fc37e8107d1cb
Nimra Abid <bsef19a022@pucit.edu.pk>
2022-04-30 15:16:12 +0500

    2nd commit

--- practiceandroidstudio/app/src/main/java/com/example/practiceandroidstudio/MainActivity.java
+++ practiceandroidstudio/app/src/main/java/com/example/practiceandroidstudio/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.practiceandroidstudio;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.button;
 
+
+import android.annotation.SuppressLint;
+import android.widget.Button;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,4 +17,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onClick(View view) {
+        Button button1 = findViewById(R.id.button1);
+
+        TextView t=findViewById(R.id.textView);
+
+button1.setText("text changed");
+t.setText("good bye");
+    }
 }
