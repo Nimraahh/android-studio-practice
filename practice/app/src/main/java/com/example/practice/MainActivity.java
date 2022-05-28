e72a4ba05f7efe2168db4e2d10c527b4d9d8fff9
Nimraahh <78551921+Nimraahh@users.noreply.github.com>
2022-05-28 18:21:15 +0500

    7th commit

---
+++ practice/app/src/main/java/com/example/practice/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b = (Button) findViewById(R.id.button);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+                {
+
+                    Uri uri =  Uri.parse("tel:+923328418357");
+                    Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                    startActivity(intent);
+                }
+
+        });
+
+
+    }
+
+
+}
