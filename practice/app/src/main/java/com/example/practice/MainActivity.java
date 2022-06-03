d4a6034be705e41d3be0a311a780ca4cdccf11a4
Nimraahh <78551921+Nimraahh@users.noreply.github.com>
2022-06-03 15:15:30 +0500

    8th commit

--- practice/app/src/main/java/com/example/practice/MainActivity.java
+++ practice/app/src/main/java/com/example/practice/MainActivity.java
@@ -1,29 +1,36 @@
 package com.example.practice;
 
+import static com.example.practice.R.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b = (Button) findViewById(R.id.button);
+    Button b;
 
+    {
+        b = (Button) findViewById(id.button);
+    }
+
+    TextView t=(TextView) findViewById(id.textView);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v)
                 {
-
-                    Uri uri =  Uri.parse("tel:+923328418357");
-                    Intent intent = new Intent(Intent.ACTION_DIAL, uri);
-                    startActivity(intent);
+                   t.setText("bye");
                 }
 
         });
